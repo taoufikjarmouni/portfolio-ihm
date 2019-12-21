ef608d7727a831a0c46281224bccfb3674ea49ab
Taoufik Jarmouni <58946927+taoufikjarmouni@users.noreply.github.com>
2019-12-21 16:28:55 +0100

    J'ajoute tous les fichiers

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll < 300){
+        $('.fixed-top').css('background', 'transparent');
+    } else{
+        $('.fixed-top').css('background', 'rgb(255, 248, 238)');
+    }
+});
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
